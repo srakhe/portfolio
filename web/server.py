da6c3dd909b7aac059f27b345ceb2f6b00321265
Sambhav Rakhe <s.rakhe@protonmail.ch>
2022-04-25 18:47:56 -0700

    About page completed

--- web/server.py
+++ web/server.py
@@ -61,4 +61,4 @@ def download(filename):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True)
--- web/utils/portfolio.py
+++ web/utils/portfolio.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 class Portfolio:
@@ -41,6 +42,8 @@ class Portfolio:
         interests = self.data.get("interests")
         languages = self.data.get("languages")
         skills = self.data.get("skills")
+        work = self.data.get("work")
+        education = self.data.get("education")
 
         return_interests = []
         if interests:
@@ -57,7 +60,50 @@ class Portfolio:
             for each_dict in skills:
                 return_skills.append(each_dict.get("name"))
 
+        return_work = []
+        if work:
+            for each_dict in work:
+                start = datetime.strptime(each_dict.get("startDate").strip(), "%Y-%m-%d")
+                end = datetime.strptime(each_dict.get("endDate").strip(), "%Y-%m-%d")
+                months_between = (end.year - start.year) * 12 + end.month - start.month + 1
+                years_between, months_between = divmod(months_between, 12)
+                duration = f"{years_between} Years {months_between} Months"
+                return_work.append({
+                    "name": each_dict.get("name"),
+                    "position": each_dict.get("position"),
+                    "start": each_dict.get("startDate"),
+                    "end": each_dict.get("endDate"),
+                    "duration": duration,
+                    "summary": each_dict.get("summary").replace("\n", " <br> "),
+                    "url": each_dict.get("url")
+                })
+
+        return_education = []
+        if education:
+            for each_dict in education:
+                start = each_dict.get("startDate")
+                end = each_dict.get("endDate")
+                if start and end:
+                    start = datetime.strptime(each_dict.get("startDate").strip(), "%Y-%m-%d")
+                    end = datetime.strptime(each_dict.get("endDate").strip(), "%Y-%m-%d")
+                    months_between = (end.year - start.year) * 12 + end.month - start.month + 1
+                    years_between, months_between = divmod(months_between, 12)
+                    duration = f"{years_between} Years {months_between} Months"
+                else:
+                    duration = f"On-Going"
+                return_education.append({
+                    "name": each_dict.get("institution"),
+                    "study": each_dict.get("studyType") + " in " + each_dict.get("area"),
+                    "start": each_dict.get("startDate"),
+                    "end": each_dict.get("endDate"),
+                    "duration": duration,
+                    "summary": each_dict.get("description").replace("\n", " <br> "),
+                    "url": each_dict.get("url")
+                })
+
         return_about["interests"] = ", ".join(return_interests)
         return_about["languages"] = ", ".join(return_languages)
         return_about["skills"] = ", ".join(return_skills)
+        return_about["work"] = return_work
+        return_about["education"] = return_education
         return return_about
