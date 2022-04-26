f134090de49f2a2cdfba81f43967840822fa3a56
Sambhav Rakhe <s.rakhe@protonmail.ch>
2022-04-25 19:56:53 -0700

    fix resume download

--- web/utils/custom.py
+++ web/utils/custom.py
@@ -6,6 +6,6 @@ class CustomUtils:
     def __init__(self, root_path):
         self.root_path = root_path
 
-    def get_file_url(self, file_name):
-        filepath = os.path.join(self.root_path, "data", file_name)
+    def get_file_url(self):
+        filepath = os.path.join(self.root_path, "data")
         return filepath
