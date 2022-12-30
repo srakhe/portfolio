c56c70eaa614d76298e0f7fb3480dbb0bb398994
Sam <rakhesambhav@gmail.com>
2022-12-30 08:52:55 -0800

    latest date fixed

--- utils/github.py
+++ utils/github.py
@@ -5,11 +5,11 @@ from datetime import datetime
 class Github:
 
     def __init__(self):
-        self.latest_update_url = "https://api.github.com/repos/srakhe/portfolio/deployments/586524427/statuses"
+        self.latest_update_url = "https://api.github.com/repos/srakhe/portfolio/commits"
 
     def get_last_update(self):
         resp = requests.get(self.latest_update_url).json()[0]
-        latest_date = resp.get("updated_at")
+        latest_date = resp.get("commit").get("author").get("date")
         if latest_date:
             latest_date = datetime.strptime(latest_date, "%Y-%m-%dT%H:%M:%SZ")
             latest_date = latest_date.strftime("%A, %d-%B-%Y")
